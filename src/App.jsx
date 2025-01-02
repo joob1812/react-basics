4ef367fb4d625a292c075e3935240fda8a559e15
*Musaa* <*diop.moussa@uam.edu.sn*>
2025-01-02 16:30:45 +0000

    Add pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import Counter from "./components/Counter";
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-  const handlClick= () => {
-    setCounter(counter + 1);
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+  const handleClick1 = () => {
+    setCounter1(counter1 + 1);
   }
+    
+  const handleClick2 = () => {
+    setCounter2(counter2 + 1);
+  }
+  
   return (
     <>
-    <Counter cpt={counter} hc={handlClick} />
-    <Counter cpt={counter} hc={handlClick} />
+    <Counter name="Counter 1" cpt={counter1} hc={handleClick1} />
+    <Counter name="Counter 2" cpt={counter2} hc={handleClick2} />
 
     </>
   )
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,8 @@
-const Counter = ({cpt, hc}) => {
+// eslint-disable-next-line react/prop-types
+const Counter = ({cpt, hc, name='Counter name'}) => {
     return (
         <div className="flex justify-between items-center m-2 bg-slate-300 p-2 rounded-lg">
-            <h1 className="text-2xl text-green-500">Counter : {cpt}</h1>
+            <h1 className="text-2xl text-green-500">{name} : {cpt}</h1>
         <button className="bg-blue-500
        hover:bg-blue-700
         text-white
